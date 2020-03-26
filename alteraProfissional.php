6e4532af5eda2136dfc8552dadbd902b93a63626
jenvieiras <jennifervsouza@icloud.com>
2020-03-25 22:52:57 -0300

    Retornando área de atuação no editar profissional e permitindo alterar a área (atualizando corretamente)

--- alteraProfissional.php
+++ alteraProfissional.php
@@ -1,15 +1,34 @@
 <?php 
-   include('classes/conexao.php');
-   session_start();
+  session_start();
     if(!isset($_SESSION['id_usuario']))
     {
       header("location: index.php");
       exit;
     }
+    // Para que esteja acessivel a todo o script
+    include('classes/conexao.php');
+    
 
+    // Suposição tá...
+    // Deposi você substitui pelos valores corretos
+    $id = $_GET['id'];
+    if ($id != ""){
+      $sql = "SELECT * FROM tb_profissional WHERE id_profissional ='$id'" ;  
+      $pesquisa = mysqli_query($link, $sql);
+      while($linhaTabela = mysqli_fetch_array($pesquisa)) {
+        $nome     = $linhaTabela[1]; // Coloquei um exemplo no campo "nome"
+        $licenca  = $linhaTabela[2];
+        $cpf      = $linhaTabela[3];
+        $rg       = $linhaTabela[4];
+        $email    = $linhaTabela[5];
+        $celular  = $linhaTabela[6];
+        $area     = $linhaTabela[7]; // campo da combo
+        $dataC    = $linhaTabela[8];
+      }
+    }
+    
    if($_SERVER['REQUEST_METHOD'] === 'POST'){
       
-      $id       = $_POST["id_profissional"];
       $nome     = $_POST["nome_profissional"];
       $licenca  = $_POST["licenca_atuacao"];
       $cpf      = $_POST["cpf_profissional"];
@@ -19,32 +38,34 @@
       $area     = $_POST["idarea"];
       $dataC    = $_POST["dt_cad_profissional"];
       
-      $sql = "UPDATE tb_profissional SET nome_profissional='" . $nome . "', idarea='" . $area . "', licenca_atuacao='" . $licenca . "', cpf_profissional='" . $cpf . "', rg_profissional='" . $rg . "', email_profissional='" . $email . "', celular_profissional='" . $celular . "', dt_cad_profissional='" . $dataC . "' WHERE id_profissional=" . $id;
+      // mudar para UPDATE SET ....
+      $sql = "UPDATE tb_profissional SETnome_profissional='" . $nome . "', 
+      idarea='" . $area . "', licenca_atuacao='" . $licenca . "', 
+      cpf_profissional='" . $cpf . "', rg_profissional='" . $rg . "', 
+      email_profissional='" . $email . "', celular_profissional='" . $celular . "', 
+      dt_cad_profissional='" . $dataC . "' WHERE id_profissional=" . $id;
       //echo ($sql);
-      mysqli_query($link, $sql);
+      
+      $result = mysqli_query($link, $sql);
       ?>
       <script type="text/javascript">location.replace("listaProfissional.php")</script>
       <?php
    }
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM tb_profissional WHERE id_profissional ='$id'" ;
-    $result = mysqli_query($link, $sql);
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<?php include('head.html'); ?>
-<body>
-<?php include('header.html'); ?>
-<?php while($linhaTabela = mysqli_fetch_array($result)){ ?>
-    <form action="alteraProfissional.php" class="prof" method="POST">
-        <h2 class="border border-secondary rounded bg-secondary text-white col-md-8">Editar profissional</h2>
+  <?php include('head.html'); ?>
+  <body>
+    <?php include('header.html'); ?>
+    <form action="cadastraProfissional.php" class="prof" method="POST">
+        <h2 class="border border-secondary rounded bg-secondary text-white col-md-8">Novo profissional</h2>
         <hr class="col-md-8" />
-        <input type="hidden" value="<?php echo($linhaTabela[0]) ?>" name="id_profissional">
         <div class="row">
             <div class="form-group col-md-8" name="nome_profissional">
                 <label for="name">Nome:</label>
-                <input type="text" class="form-control" value="<?php echo($linhaTabela[1]) ?>" name="nome_profissional">
+                <?php /* Exemplo para escrever no campo caso exista um resultado para o valor de pesquisa */ ?>
+                <input type="text" class="form-control" name="nome_profissional" value="<?php if ($nome != '') { echo $nome; } ?>"> 
             </div>
 
         </div>
@@ -53,47 +74,60 @@
                 <label for="name">Área de atuação:</label>
                 <select class="form-control browser-default custom-select" 
                         name="idarea" style="width: 100%;">
-                    <option <?php echo $linhaTabela[7]=='1'? ('selected ') :'' ?> value="1">Fisioterapia</option>
-                    <option <?php echo $linhaTabela[7]=='2'? ('selected ') :''  ?>value="2">Fonoaudiologia</option>
-                    <option <?php echo $linhaTabela[7]=='3'? ('selected ') :''  ?>value="3">Nutrição</option>
-                    <option <?php echo $linhaTabela[7]=='4'? ('selected ') :''  ?>value="4">Psicologia</option>
+                    <option value="0">Selecione sua área de atuação</option>
+                    <?php 
+                    // Selecionar dados da tabela de área
+                      $sqlArea = "SELECT * FROM tb_area";
+                      $resultado = mysqli_query($link, $sqlArea);
+                    // Ler resultados da tabela e escrever na combobox
+                        while($linhaTabela = mysqli_fetch_array($resultado)){ 
+                          // Se o código da área for o mesmo que está no resultado ele aparece selecionado
+                          if ( $area == $linhaTabela[0] ) {
+                            echo '<option value="' . $linhaTabela[0] . '" selected>' . $linhaTabela[1] . '</option>';
+                          }
+                          // Se não for aparece na lista normalmente para ser selecionado
+                          else {
+                            echo '<option value="' . $linhaTabela[0] . '" >' . $linhaTabela[1] . '</option>';
+                          }
+                        }
+                    ?>
                 </select>  
             </div>
             <div class="form-group col-md-4" name="licenca_atuacao">
                 <label for="name">Licença profissional:</label>
-                <input type="text" class="form-control" value="<?php echo($linhaTabela[2]) ?>" name="licenca_atuacao">
+                <input type="text" class="form-control" name="licenca_atuacao" value="<?php if ($licenca != '') { echo $licenca; } ?>">
             </div>
         </div>
         <div class="row">
             <div class="form-group col-md-4" name="cpf_profissional">
                 <label for="name">CPF:</label>
-                <input type="text" class="form-control" value="<?php echo($linhaTabela[3]) ?>" name="cpf_profissional">
+                <input type="text" class="form-control" name="cpf_profissional" value="<?php if ($cpf != '') { echo $cpf; } ?>">
             </div>
             <div class="form-group col-md-4" name="rg_profissional">
                 <label for="name">RG:</label>
-                <input type="text" class="form-control" value="<?php echo($linhaTabela[4]) ?>" name="rg_profissional">
+                <input type="text" class="form-control" name="rg_profissional" value="<?php if ($rg != '') { echo $rg; } ?>">
             </div>
         </div>
         <div class="row">
             <div class="form-group col-md-8" name="email_profissional">
                 <label for="name">E-mail:</label>
-                <input type="text" class="form-control" value="<?php echo($linhaTabela[5]) ?>" name="email_profissional">
+                <input type="text" class="form-control" name="email_profissional" value="<?php if ($email != '') { echo $email; } ?>">
             </div>
         </div>
         <div class="row">
             <div class="form-group col-md-4" name="celular_profissional">
                 <label for="name">Celular:</label>
-                <input type="text" class="form-control" value="<?php echo($linhaTabela[6]) ?>" name="celular_profissional">
+                <input type="text" class="form-control" name="celular_profissional" value="<?php if ($celular != '') { echo $celular; } ?>">
             </div>
             <div class="form-group col-md-4" name="dt_cad_profissional">
                 <label for="name">Data do cadastro:</label>
-                <input type="date" class="form-control" value="<?php echo($linhaTabela[8]) ?>" name="dt_cad_profissional">
+                <input type="date" class="form-control" name="dt_cad_profissional" value="<?php if ($dataC != '') { echo $dataC; } ?>">
             </div>
         </div>
         <hr class="col-md-8" />
         <div class="row btn-toolbar" role="toolbar" style="padding-left: 50%;">
             <div class="btn-group mr-2" role="group">
-              <input type="button" class="btn btn-danger" value="Cancelar">
+              <input type="submit" class="btn btn-danger" value="Cancelar">
             </div>
             <div class="btn-group mr-2" role="group">
               <input type="submit" class="btn btn-success" value="Salvar">
@@ -101,6 +135,5 @@
         </div>
         <hr class="col-md-8" />
     </form>
-<?php } ?>
-</body>
+  </body>
 </html>
